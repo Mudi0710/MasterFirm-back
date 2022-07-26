9cedd56b65020a306a01b4191baf78ae370cc6ce
Mudi0710 <nomis820710@gmail.com>
2022-07-26 16:07:54 +0800

    建立-models/products.js：設定商品資料模型
    設定商品資料模型

---
+++ models/products.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, '商品名稱為必填']
+  },
+  description: {
+    type: String
+  },
+  sell: {
+    // true = 上架
+    // false = 下架
+    type: Boolean,
+    default: false
+  },
+  inventory: {
+    type: Number,
+    required: [true, '商品名稱為必填']
+  },
+  price: {
+    type: Number,
+    required: [true, '商品價格為必填']
+  },
+  image: {
+    type: String
+  }
+}, { versionKey: false })
+
+export default mongoose.model('products', schema)
