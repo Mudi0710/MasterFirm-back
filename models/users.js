2ee65199cf974ef1683bc5177061b8d8f68d3ac6
Mudi0710 <nomis820710@gmail.com>
2022-08-10 15:42:36 +0800

    修改-models/users.js：修改使用者資料模型
    修改使用者資料模型

--- models/users.js
+++ models/users.js
@@ -8,7 +8,7 @@ const schema = new mongoose.Schema({
     minlength: [4, '帳號至少 4 個字元'],
     maxlength: [20, '帳號最多 20 個字元'],
     unique: true,
-    match: [/^(?=.*[a-z])(?=.*\d)[a-z\d]{4,20}$/, '帳號格式錯誤，須至少包含一個英文小寫及一個數字']
+    match: [/^[a-z0-9]+$/, '格式錯誤，只能由小寫英文及數字組成']
   },
   password: {
     type: String,
@@ -16,9 +16,9 @@ const schema = new mongoose.Schema({
   },
   name: {
     type: String,
-    required: [true, '真實姓名為必填'],
-    minlength: [2, '真實姓名至少 2 個字以上'],
-    match: [/^[\u4e00-\u9fa5]{2,}$/, '真實姓名格式錯誤']
+    required: [true, '會員姓名為必填'],
+    minlength: [2, '會員姓名至少 2 個字以上'],
+    match: [/^[\u4e00-\u9fa5]{2,}$/, '會員姓名格式錯誤，應為中文姓名']
   },
   gender: {
     // 0 = 未選擇
@@ -49,7 +49,7 @@ const schema = new mongoose.Schema({
     minlength: [12, '手機號碼為 10 個數字'],
     maxlength: [12, '手機號碼為 10 個數字'],
     unique: true,
-    match: [/^09[0-9]{2}.[0-9]{3}.[0-9]{3}$/, '手機號碼格式錯誤']
+    match: [/^09[0-9]{2}.[0-9]{3}.[0-9]{3}$/, '手機號碼格式錯誤，應為 09 開頭']
   },
   address: {
     type: String
