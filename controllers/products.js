1450720613efd4883e9ed4eee6598728cb567cf0
Mudi0710 <nomis820710@gmail.com>
2022-08-14 23:40:25 +0800

    修改-controllers/products.js：修改 products 控制器
    修改 products 控制器

---
+++ controllers/products.js
@@ -0,0 +1,81 @@
+import products from '../models/products.js'
+
+// 新增商品
+export const createProduct = async (req, res) => {
+  try {
+    const result = await products.create({
+      name: req.body.name,
+      description: req.body.description,
+      sell: req.body.sell,
+      inventory: req.body.inventory,
+      price: req.body.price,
+      // 加上問號(可選串連)是因為：圖片可能沒上傳，所以可能會沒有圖片 => req.file 就會是 undefined，對 undefined 的東西加上 .path 會出現錯誤，所以才加問號讓程式可以忽略並繼續執行
+      image: req.file?.path || ''
+    })
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      return res.status(400).send({ success: false, message })
+    } else {
+      res.status(500).send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+}
+
+// 抓到所有已上架商品，所有人都能看
+export const getProducts = async (req, res) => {
+  try {
+    const result = await products.find({ sell: true })
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
+
+// 抓到所有商品（包含下架），只有管理者能看到
+export const getAllProducts = async (req, res) => {
+  try {
+    const result = await products.find()
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
+
+// 抓到單一個商品，所有人都能看
+export const getProduct = async (req, res) => {
+  try {
+    // params 是路由參數
+    const result = await products.findById(req.params.id)
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
+
+// 編輯商品，只有管理者能編輯
+export const editProduct = async (req, res) => {
+  try {
+    const data = {
+      name: req.body.name,
+      description: req.body.description,
+      sell: req.body.sell,
+      inventory: req.body.inventory,
+      price: req.body.price
+    }
+    // 如果 req.file 不是空的
+    if (req.file) data.image = req.file.path
+    const result = await products.findByIdAndUpdate(req.params.id, data, { new: true })
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      return res.status(400).send({ success: false, message })
+    } else {
+      res.status(500).send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+}
