16a9c3a4144d3129e5c22daaaa3a03d3398d431c
Mudi0710 <nomis820710@gmail.com>
2022-08-24 14:59:49 +0800

    修改-models/newses.js：修改消息頁資料模型
    修改消息頁資料模型

--- models/newses.js
+++ models/newses.js
@@ -3,25 +3,21 @@ import mongoose from 'mongoose'
 const schema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, '文章標題為必填']
+    required: [true, '消息標題為必填']
   },
   content: {
     type: String,
-    required: [true, '文章內容為必填']
+    required: [true, '消息內容為必填']
   },
   date: {
     type: Date,
-    required: true,
     default: Date.now()
   },
   show: {
-    // true = 上架
-    // false = 下架
+    // true = 已發布
+    // false = 未發布
     type: Boolean,
     default: false
-  },
-  image: {
-    type: [String]
   }
 }, { versionKey: false })
 
