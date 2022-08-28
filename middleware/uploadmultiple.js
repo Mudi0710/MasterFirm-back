1187aecf25876f856def2182b1d9809a75a1ee8b
Mudi0710 <nomis820710@gmail.com>
2022-08-28 19:47:41 +0800

    建立-middleware/uploadmultiple.js：建立 uploadmultiple 驗證
    建立 uploadmultiple 驗證

---
+++ middleware/uploadmultiple.js
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from 'cloudinary'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import multer from 'multer'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+})
+
+const upload = multer({
+  storage: new CloudinaryStorage({ cloudinary }),
+  fileFilter (req, file, cb) {
+    if (!file.mimetype.startsWith('image')) {
+      cb(new multer.MulterError('LIMIT_FORMAT'), false)
+    } else {
+      cb(null, true)
+    }
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 2
+  }
+})
+
+export default async (req, res, next) => {
+  upload.fields([{ name: 'indexImageMobile', maxCount: 5 }, { name: 'indexImageDesktop', maxCount: 5 }, { name: 'newsImageMobile', maxCount: 5 }, { name: 'newsImageDesktop', maxCount: 5 }, { name: 'knowledgesImageMobile', maxCount: 5 }, { name: 'knowledgesImageDesktop', maxCount: 5 }, { name: 'casesImageMobile', maxCount: 5 }, { name: 'casesImageDesktop', maxCount: 5 }])(req, res, async error => {
+    if (error instanceof multer.MulterError) {
+      let message = '上傳失敗'
+      if (error.code === 'LIMIT_FILE_SIZE') {
+        message = '檔案太大，超過 2 MB 限制'
+      } else if (error.code === 'LIMIT_FORMAT') {
+        message = '檔案格式錯誤，非圖片檔'
+      }
+      res.status(400).send({ success: false, message })
+    } else if (error) {
+      res.status(500).send({ success: false, message: '伺服器錯誤' })
+    } else {
+      // console.log(req.files.indexImageMobile)
+      next()
+    }
+  })
+}
